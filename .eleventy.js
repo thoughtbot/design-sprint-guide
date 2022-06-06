215533042c8b16422a11af24625c004ef257bc7d
LkeMitchll <mail@luke.fail>
2022-06-06 11:51:15 +0100

    Update dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -68,7 +68,6 @@ module.exports = function (eleventyConfig) {
 
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addWatchTarget("./assets/sass");
   eleventyConfig.addPassthroughCopy("./css");
 
