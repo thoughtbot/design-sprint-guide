440e0a0e58d4192a32373c1c5d20f5afffe1dbb9
LkeMitchll <mail@luke.fail>
2022-06-08 14:11:19 +0100

    Add primary heading and baseline typography

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   // Watch scss fuiles for changes
-  eleventyConfig.addWatchTarget("./assets/sass");
+  eleventyConfig.addWatchTarget("_includes/assets/sass");
   // Copy the `css` folders to the output
   eleventyConfig.addPassthroughCopy("./css");
-}
+  eleventyConfig.addPassthroughCopy({ "_includes/assets/fonts": "fonts" });
+};
