c69ffb0cb9655d4005c28ef65bfeff8a806f048a
LkeMitchll <mail@luke.fail>
2022-10-27 10:58:00 +0100

    Sort phases by 'number' frontmatter key, not date

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   // Plugin that renders other languages inside templates
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   // Watch scss files for changes
@@ -9,4 +9,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./css");
   eleventyConfig.addPassthroughCopy({ "_includes/assets/fonts": "fonts" });
   eleventyConfig.addPassthroughCopy({ "_includes/assets/images": "images" });
+  // Phases collection: Sorted by "number" key in frontmatter
+  eleventyConfig.addCollection("phase", function(collectionApi) {
+    return collectionApi.getFilteredByTag("phase").sort(function(a, b) {
+      return a.data.number - b.data.number
+    });
+  });
 };
