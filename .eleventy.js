61143473659968805ebc3d2ba4434bfe06b094e5
LkeMitchll <mail@luke.fail>
2022-10-27 10:33:06 +0100

    Add introductions to phases via render plugin
    Each phase now has an accompaying `_introduction.md` file, which is
    rendered in the index via the built-in render plugin:
    
    https://www.11ty.dev/docs/plugins/render/#renderfile

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
-  // Watch scss fuiles for changes
+  // Plugin that renders other languages inside templates
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+  // Watch scss files for changes
   eleventyConfig.addWatchTarget("_includes/assets/sass");
-  // Copy the `css` folders to the output
+  // Copy the asset folders to the output
   eleventyConfig.addPassthroughCopy("./css");
   eleventyConfig.addPassthroughCopy({ "_includes/assets/fonts": "fonts" });
   eleventyConfig.addPassthroughCopy({ "_includes/assets/images": "images" });
