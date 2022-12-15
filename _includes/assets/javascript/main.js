940dd5b83373eea66fb6c697211411a18afd844c
Dave Novak <101594821+davidrnovak@users.noreply.github.com>
2022-12-15 13:17:20 -0800

    add js

--- _includes/assets/javascript/main.js
+++ _includes/assets/javascript/main.js
@@ -1,15 +1,15 @@
-const tabs = document.querySelectorAll('.tab-radio');
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll('.tab-radio');
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (event) => {
-    window.location.hash = "#" + event.target.value
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", (event) => {
+      window.location.hash = "#" + event.target.value
+    });
   });
-});
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  let hash = window.location.hash.split("#")[1]
-  let tab = document.querySelector(`.tab-radio[value='${hash}'`)
+  const hash = window.location.hash.split("#")[1]
+  const tab = document.querySelector(`.tab-radio[value='${hash}'`)
   if(tab) {
     tab.checked = true
   }
-})
+});
