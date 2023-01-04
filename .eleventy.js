736591ccaa905292dbaebf0eaef2cde9d42e9cbf
Fatima Burke <fatimaburke@fatimaswordmbp.mynetworksettings.com>
2023-01-04 12:40:43 -0500

    added introduction field in CMS to schedule, installed markdown-it pa ckage, and created shortcode to render introductions as markdown on view

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const markdown = require("markdown-it")({
+  html: true
+})
 
 module.exports = function(eleventyConfig) {
+  // shortcode to convert incoming markdown from CMS in data files to HTML inline
+  eleventyConfig.addFilter('markdown', value => {
+    return `<div class="md-block">${markdown.render(value)}</div>`    
+  })
 
   // Plugin that renders other languages inside templates
   eleventyConfig.addPlugin(EleventyRenderPlugin);
@@ -40,4 +47,5 @@ module.exports = function(eleventyConfig) {
   return {
     markdownTemplateEngine: "njk"
   }
+
 };
